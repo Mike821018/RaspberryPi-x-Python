9738dd88a070b48142f996167e50ecaeaae90cda
Mike821018 <hasndsome@gmail.com>
2016-07-27 17:30:36 +0800

    Create Scaning_BTdev.py

---
+++ Scaning_BTdev.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import commands
+from threading import Thread
+from time import sleep
+
+def scanning():
+    commands.getoutput('sudo hcitool lescan')
+    
+def watching():
+    return commands.getoutput('sudo hcidump')
+    
+def stop():
+    commands.getoutput('
+# Not finished yet
